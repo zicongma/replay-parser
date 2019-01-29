6b9719027975d5c77879c24e831ab25dcd55034e
Zicong Ma <zm1216@ic.ac.uk>
2019-01-29 20:58:51 +0000

    initial commit: now output all hero related information

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import skadistats.clarity.model.Entity;
+import skadistats.clarity.model.FieldPath;
+import skadistats.clarity.processor.entities.OnEntityUpdated;
+import skadistats.clarity.processor.entities.UsesEntities;
+import skadistats.clarity.processor.runner.Context;
+import skadistats.clarity.processor.runner.SimpleRunner;
+import skadistats.clarity.source.MappedFileSource;
+
+@UsesEntities
+public class Main {
+
+    private boolean isHero(Entity e) {
+        return e.getDtClass().getDtName().startsWith("CDOTA_Unit_Hero");
+    }
+
+    @OnEntityUpdated
+    public void onUpdated(Context ctx, Entity e, FieldPath[] updatedPaths, int updateCount) {
+        if (!isHero(e)) {
+            return;
+        }
+        for (int i = 0; i < updateCount; i++) {
+            FieldPath path = updatedPaths[i];
+            String pathName = e.getDtClass().getNameForFieldPath(path);
+            System.out.format("Hero: %s, Property: %s, Value: %s, Tick: %s\n", e.getDtClass().getDtName(), pathName, e.getPropertyForFieldPath(path), ctx.getTick());
+        }
+    }
+
+
+    public void run(String[] args) throws Exception {
+        new SimpleRunner(new MappedFileSource(args[0])).runWith(this);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().run(args);
+    }
+
+}
